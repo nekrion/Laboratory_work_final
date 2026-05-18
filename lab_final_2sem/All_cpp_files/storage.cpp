7518c8d2619d0ec69f351ea97acdf416976afd8b
nekrion <nekri0nn@gmail.com>
2026-05-18 20:47:15 +0300

    Создание базы для продолжения реализции ТЗ
    Создал классы Deliver, Order, Storage, Database. Подробнее про их содержимое написано в комментариях в коде.

---
+++ lab_final_2sem/All_cpp_files/storage.cpp
@@ -0,0 +1,2 @@
+#include "../All_hpp_files/storage.hpp"
+// здесь реализация классов/класса
---
+++ lab_final_2sem/main.cpp
@@ -0,0 +1,12 @@
+#include "database.hpp"
+
+
+int main() {
+
+    // здесь реализация менюшки
+
+
+
+
+    return 0;
+}
