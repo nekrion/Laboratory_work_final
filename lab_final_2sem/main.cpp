8d5f25636ca351a164e11306b542b1d0fea4a7bb
antihasap <124263660+antihasap@users.noreply.github.com>
2026-05-22 01:10:02 +0300

    Реализация вывода с координатами
    тут надо будет немного доделать, назначение заказов работает криво

--- lab_final_2sem/main.cpp
+++ lab_final_2sem/main.cpp
@@ -3,39 +3,15 @@
 #include "deliver.hpp"
 #include "order.hpp"
 #include "storage.hpp"
+#include "manager.hpp"
+#include <queue>
+#include <cmath>
 
 void LoadTerminal() {
-
     cout << "Запуск терминала";
-    this_thread::sleep_for(chrono::seconds(3));
-    for (int i = 0; i < 3; i++) {
-        cout << "." << flush;
-        this_thread::sleep_for(chrono::seconds(1));
-    }
-
-    for (int i = 0; i <= 100; i += 10) {
-        cout << "\rLoading: [";
-        int progress = i / 10;
-
-        for (int j = 0; j < 10; ++j) {
-            if (j < progress) {
-                cout << "#";
-            } else {
-                cout << " ";
-            }              
-        }
-        
-        cout << "] " << i << "%" << flush;
-        
-        if(i <= 50) {
-            this_thread::sleep_for(chrono::milliseconds(150));
-        } else {
-            this_thread::sleep_for(chrono::milliseconds(500));
-        }
-    }
-    
-    cout << "\nТерминал запущен\n"; 
-    this_thread::sleep_for(chrono::seconds(2));
+    this_thread::sleep_for(chrono::seconds(1));
+    cout << "\nТерминал запущен\n";
+    this_thread::sleep_for(chrono::seconds(1));
     system("cls");
 }
 
@@ -46,54 +22,96 @@ void ShowTerminal() {
     cout << "Для взаимодействия с меню Вы можете использовать следующие команды:\n";
     cout << "0 - Выход из терминала\n";
     cout << "1 - Вывод БД на экран\n";
-    cout << "2 - Удаление элемента из БД\n";
-    cout << "3 - Сохранение БД в файл\n";
-    cout << "4 - Загрузка из файла\n";
-    cout << "5 - Перемотка времени\n";
+    cout << "2 - Добавить тестовый заказ\n";
+    cout << "3 - Назначить заказы (вручную)\n";
+    cout << "4 - Перемотка времени\n";
+    cout << "5 - Показать статус курьеров\n";
+    cout << "6 - Показать очередь заказов\n";
     cout << "||===========================================================||\n";
 }
 
-
 int main() {
-
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-    // здесь реализация менюшки
-    // ну и еще заполнение
-    // также какое-то тестовое заполнение для теста функций можно
+    LoadTerminal();
 
-    LoadTerminal(); // запуск терминала
-    
-    ShowTerminal(); // показывает стандартное окно терминала
+    Database db;
+    DeliveryManager manager(db);
 
+    // первое назначение заказов
+    manager.assignOrders();
 
-    queue<Order> all_orders; // очередь заказов
-    int k;
-    int time = 0;
+    int choice;
+    while (true) {
+        ShowTerminal();
+        cout << "\nТекущее время: " << static_cast<int>(manager.getCurrentTime()) << " часов\n";
+        cout << "Ваш выбор: ";
 
-    if (!(cin >> k)) {
-        cout << "!      Input_Error      !\n";
-        cout << "Brake up";
-        cin.clear();
-        cin.ignore(10000, '\n');
-        return 1;
-
-    } else {
-        while (k != 0) {
-            switch(k) {
-                case 0: {
-                    cout << "Спасибо за пользование терминалом!\n";
-                    break;
-                }
-                case 1: {
-                    
-                }
-            }
+        if (!(cin >> choice)) {
+            cout << "Ошибка ввода!\n";
+            cin.clear();
+            cin.ignore(10000, '\n');
+            continue;
         }
-        
-    }
 
+        switch (choice) {
+        case 0:
+            cout << "Спасибо за пользование терминалом!\n";
+            return 0;
+
+        case 1:
+            db.printAll();
+            break;
+
+        case 2: {
+            static int test_id = 200;
+            string name;
+            int x, y;
+            cout << "Имя клиента: ";
+            cin >> name;
+            cout << "Координата X: ";
+            cin >> x;
+            cout << "Координата Y: ";
+            cin >> y;
+            cout << "ID склада: ";
+            int storage_id;
+            cin >> storage_id;
+
+            Order new_order(test_id++, name, { x, y }, storage_id);
+            manager.addOrderToQueue(new_order);
+            break;
+        }
+
+        case 3:
+            manager.assignOrders();
+            break;
+
+        case 4: {
+            int hours;
+            cout << "На сколько часов перемотать время? ";
+            cin >> hours;
+            manager.fastForward(hours);
+            break;
+        }
+
+        case 5:
+            manager.printDeliverStatus();
+            break;
+
+        case 6:
+            manager.printOrderQueue();
+            break;
+
+        default:
+            cout << "Неверная команда!\n";
+        }
+
+        cout << "\nНажмите Enter для продолжения...";
+        cin.ignore();
+        cin.get();
+        system("cls");
+    }
 
     return 0;
 }
