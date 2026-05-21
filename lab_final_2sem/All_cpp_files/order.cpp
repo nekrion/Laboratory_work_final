cfb5ed1c57618699766bf8841eeee1a3f4c48922
antihasap <124263660+antihasap@users.noreply.github.com>
2026-05-22 00:58:03 +0300

    Реализация расстояния до склада по координатам
    очень много кода перелопатил, пока адаптировал всё под новые методы, нужно будет доделать вывод и переписать некоторые функции, например сейчас у меня не задается заказ, но добавить его можно

--- lab_final_2sem/All_cpp_files/order.cpp
+++ lab_final_2sem/All_cpp_files/order.cpp
@@ -2,134 +2,46 @@
 // здесь реализация классов/класса
 
 
-//constructors
-Order::Order(unsigned int id, unsigned int summary, int date_accepted, int date_delivered, 
-          const string &place, const string &storage, bool is_accepted, bool is_delivered, const pair<int, int> &place_coordinates) {
-    
-    this->id = id;
-    this->id_deliver = 0;
-    this->summary = summary;
-    this->date_accepted = date_accepted;
-    this->date_delivered = date_delivered;
-    this->place = place;
-    this->storage = storage;
-    this->is_accepted = is_accepted;
-    this->is_delivered = is_delivered;
-    this->place_coordinates = place_coordinates;
-}   
-
-
-
-//getters
-unsigned int Order::getId() const {
-    return id;
+// конструкторы
+Order::Order(unsigned int id, const string& name, const pair<int, int>& addr,
+    unsigned int storage_id)
+    : id(id), customer_name(name), place(""), storage_id(storage_id),
+    delivery_address(addr), is_completed(false), is_active(false),
+    assigned_deliver_id(0), distance_from_storage(0), estimated_time(0) {
+    // Расстояние пока 0, будет вычислено при назначении
 }
 
-unsigned int Order::getIdDeliver() const {
-    return id_deliver;
+unsigned int Order::getId() const { return id; }
+string Order::getCustomerName() const { return customer_name; }
+pair<int, int> Order::getDeliveryAddress() const { return delivery_address; }
+unsigned int Order::getStorageId() const { return storage_id; }
+bool Order::getIsCompleted() const { return is_completed; }
+bool Order::getIsActive() const { return is_active; }
+unsigned int Order::getAssignedDeliverId() const { return assigned_deliver_id; }
+double Order::getDistance() const { return distance_from_storage; }
+double Order::getEstimatedTime() const { return estimated_time; }
+
+void Order::setIsCompleted(bool completed) { is_completed = completed; }
+void Order::setIsActive(bool active) { is_active = active; }
+void Order::setAssignedDeliverId(unsigned int deliver_id) { assigned_deliver_id = deliver_id; }
+void Order::setDistance(double dist) { distance_from_storage = dist; }
+void Order::setEstimatedTime(double time) { estimated_time = time; }
+
+double Order::calculateDistanceTo(const pair<int, int>& point) const {
+    double dx = delivery_address.first - point.first;
+    double dy = delivery_address.second - point.second;
+    return sqrt(dx * dx + dy * dy);
 }
 
-unsigned int Order::getSummary() const {
-    return summary;
-}
-
-int Order::getDateAccepted() const {
-    return date_accepted;
-}
-
-int Order::getDateDelivered() const {
-    return date_delivered;
-}
-
-const string& Order::getPlace() const {
-    return place;
-}
-
-const string& Order::getStorage() const {
-    return storage;
-}
-
-bool Order::getIsAccepted() const {
-    return is_accepted;
-}
-
-bool Order::getIsDelivered() const {
-    return is_delivered;
-}
-
-const pair<int, int>& Order::getPlaceCoordinates() const {
-    return place_coordinates;
-}
-
-
-//setters
-void Order::setId(unsigned int n_id) {
-    id = n_id;
-}
-
-void Order::setIdDeliver(unsigned int n_id_deliver) {
-    id_deliver = n_id_deliver;
-}
-
-void Order::setSummary(unsigned int n_summary) {
-    summary = n_summary;
-}
-
-void Order::setDateAccepted(int n_date_accepted) {
-    date_accepted = n_date_accepted;
-}
-
-void Order::setDateDelivered(int n_date_delivered) {
-    date_delivered = n_date_delivered;
-}
-
-void Order::setPlace(string &n_place) {
-    place = n_place;
-}
-
-void Order::setStorage(string &n_storage) {
-    storage = n_storage;
-}
-
-void Order::setIsAccepted(bool n_is_accepted) {
-    is_accepted = n_is_accepted;
-}
-
-void Order::setIsDelivered(bool n_is_delivered) {
-    is_delivered = n_is_delivered;
-}
-
-void Order::setPlaceCoordinates(pair<int, int> &n_place_coordinates) {
-    place_coordinates = n_place_coordinates;
-}
-
-
-//methods
-void Order::print() const{
-    cout << "--------------------------\n";
-    cout << "Тип: Заказ\n";
-    cout << "ID: " << id << "\n";
-    if (id_deliver == 0) {
-        cout << "Курьер не назначен\n";
-    } else {
-        cout << "ID курьера, назанченного на данный заказ: " << id_deliver << "\n";
-    }
-    cout << "Сумма: " << summary << "\n";
-    cout << "Время принятия заказа: " << date_accepted << "\n";
-    cout << "Время доставки: " << date_delivered << "\n";
-    cout << "Откуда: " << storage << "\n";
-    cout << "Куда: " << place << "( x: " << place_coordinates.first << " y: " << place_coordinates.second << ")\n";
-    if (is_accepted) {
-        cout << "Заказ принят\n";
-    } else {
-        cout << "Заказ не принят\n";
-    }
-    if (is_delivered) {
-        cout << "Заказ доставлен\n";
-    } else {
-        cout << "Заказ не доставлен\n";
+void Order::print() const {
+    cout << "Заказ ID: " << id << "\n";
+    cout << "Клиент: " << customer_name << "\n";
+    cout << "Адрес: (" << delivery_address.first << ", " << delivery_address.second << ")\n";
+    cout << "Склад ID: " << storage_id << "\n";
+    cout << "Расстояние до склада: " << distance_from_storage << "\n";
+    cout << "Статус: " << (is_completed ? "Выполнен" : (is_active ? "В обработке" : "В очереди")) << "\n";
+    if (is_active) {
+        cout << "Курьер ID: " << assigned_deliver_id << "\n";
     }
     cout << "--------------------------\n";
-
-    
 }
